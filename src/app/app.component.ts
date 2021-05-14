314be8b7eae0328b06160e4a97f5169680141cb1
yothinn <e20utt@hotmail.com>
2021-05-14 13:36:56 +0700

    add html2canvas and download image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
   title = 'shareholder-certificate';
+
+  @ViewChild('certificate') certificate: ElementRef;
+  @ViewChild('canvas') canvas:ElementRef;
+  @ViewChild('downloadLink') downloadLink:ElementRef;
 
   shareholderData = {
     shareholderId: '2563/0001',                                 // ทะเบียนผู้ถือหุ้น
@@ -25,5 +31,15 @@ export class AppComponent {
     let month = ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน',
                   'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'];
     return `${d.getDate()} ${month[d.getMonth()]} ${d.getFullYear()+543}`
+  }
+
+
+  onDownload() {
+    html2canvas(this.certificate.nativeElement).then(canvas => {
+      this.canvas.nativeElement.src = canvas.toDataURL();
+      this.downloadLink.nativeElement.href = canvas.toDataURL('image/jpg');
+      this.downloadLink.nativeElement.download = `${this.shareholderData.certificateId}_${this.shareholderData.firstName} ${this.shareholderData.lastName}.jpg`;
+      this.downloadLink.nativeElement.click();
+    })
   }
 }
