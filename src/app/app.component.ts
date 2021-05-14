56c49983af91c5bea1c3c3424176d813fe6beaf5
yothinn <e20utt@hotmail.com>
2021-05-14 13:16:39 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shareholder-certificate';
+
+  shareholderData = {
+    shareholderId: '2563/0001',                                 // ทะเบียนผู้ถือหุ้น
+    certificateId: '2563/0002',                                 // เลขที่ใบหุ้น
+    title: 'นาย',                                               // คำนำหน้า
+    firstName: 'ธีรศักดิ์',                                         // ชื่อ
+    lastName: 'ทับฤทธิ์',                                         // นามสกุล
+    numShare: 100,                                              // จำนวนหุ้น
+    valueShare: 10000,                                          // ค่าหุ้น
+    registerDate: '2021-05-14T17:00:00.000+00:00'               // ISO Date
+  }
+
+
+  getThaiDate(date): string {
+    let d = new Date(date);
+    let month = ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน',
+                  'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'];
+    return `${d.getDate()} ${month[d.getMonth()]} ${d.getFullYear()+543}`
+  }
 }
